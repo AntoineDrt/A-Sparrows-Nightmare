66b48b3c67d31797fb4cdba455e95ba9ac811396
YannLescot <yann.lescot@hotmail.com>
2024-03-25 16:15:38 +0100

    ✨ Menu de pause implémenté et ajouté aux scenes de jeu

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    private Canvas PauseMenuCanvas;
+    private Boolean isPaused = false;
+
+    void Start()
+    {
+        PauseMenuCanvas = GameObject.Find("PauseMenu").GetComponent<Canvas>();
+        PauseMenuCanvas.enabled = false;   
+    }
+
+    public void PauseGame(bool currentlyPaused)
+    {
+        Time.timeScale = currentlyPaused ? 1 : 0;
+        PauseMenuCanvas.enabled = currentlyPaused ? false : true;
+
+        isPaused = !isPaused;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame(isPaused);
+        }   
+    }
+}
